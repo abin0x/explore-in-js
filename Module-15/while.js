4d059329c3bf4685522a89606ec07e88bc45e962
abin0x <mahmudulabin@gmail.com>
2025-08-22 21:48:54 +0600

    while loop

---
+++ Module-15/while.js
@@ -0,0 +1,16 @@
+const number=7;
+if(number<10)
+{
+    console.log('Number ta 10 ar cheye choto');
+}
+console.log("Number ta boro");
+
+
+// ----------------While loop-----------------
+
+let num = 0;
+
+while (num < 10) {
+  console.log(`Number is : ${num}`);
+  num++;
+}
