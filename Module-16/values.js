75de4e0b5bce519d66a489db0a9139c4093c4e0d
abin0x <mahmudulabin@gmail.com>
2025-08-24 12:03:38 +0600

    values

---
+++ Module-16/values.js
@@ -0,0 +1,16 @@
+const Computer={
+    brand: "Dell",
+    model: "XPS 13",
+    year: 2020,
+    color: "Silver",
+    specifications: {
+        processor: "Intel Core i7",
+        ram: "16GB",
+        storage: "512GB SSD"
+    }
+
+}
+const keys=Object.keys(Computer);
+console.log(keys);
+const values=Object.values(Computer);
+console.log(values);
