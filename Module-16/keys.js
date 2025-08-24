217fbecf95a4800a536673b317128344bad69179
abin0x <mahmudulabin@gmail.com>
2025-08-24 12:02:37 +0600

    Keys

---
+++ Module-16/keys.js
@@ -0,0 +1,14 @@
+const Computer={
+    brand: "Dell",
+    model: "XPS 13",
+    year: 2020,
+    color: "Silver",
+    specifications: {
+        processor: "Intel Core i7",
+        ram: "16GB",
+        storage: "512GB SSD"
+    }
+
+}
+const keys=Object.keys(Computer);
+console.log(keys);
