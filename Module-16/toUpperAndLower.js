2a6f7ba4066dd27ace2ea1d54112f24a1f16bd9f
abin0x <mahmudulabin@gmail.com>
2025-08-24 11:40:07 +0600

    Toupper

---
+++ Module-16/toUpperAndLower.js
@@ -0,0 +1,12 @@
+const sub='Chemistry';
+const subject='chemistry';
+console.log(sub.toUpperCase());
+console.log(subject.toLowerCase());
+if(sub.toLowerCase()===subject.toLowerCase())
+{
+    console.log("Both strings are equal");
+}
+else 
+{
+    console.log("Both strings are not equal");
+}
